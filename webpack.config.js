794ba9e3b1b216261fe5fbf9f9e7963ea5d55e10
Johnpaul <bohrplus@gmail.com>
2018-11-21 17:59:27 +0100

    Set up

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+export const entry = './src/index.js';
+export const output = {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+};
+export const module = {
+    loaders: [
+        {
+            test: /\.json$/,
+            loader: 'json-loader'
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }
+    ]
+};
